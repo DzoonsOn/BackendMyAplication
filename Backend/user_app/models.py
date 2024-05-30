0e14d7111a214e1ff391ca04b73c49680ae3929b
Jan Maksymiuk <113555449+DzoonsOn@users.noreply.github.com>
2024-05-30 12:25:30 +0200

    Update file models.py

--- Backend/user_app/models.py
+++ Backend/user_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django_resized import ResizedImageField
+
 
 # Create your models here.
+def upload_image_path(instance, filename):
+    return "/profile/" + str(filename)
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=55)
+    profile_picture = ResizedImageField(upload_to='profile_pictures', blank=True, null=True)
